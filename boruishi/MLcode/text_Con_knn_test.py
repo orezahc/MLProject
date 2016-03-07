8ca54d60b8e49bffbcd0282fd996da482314b025
orezahc <chazerod@gmail.com>
2016-03-06 19:04:57 -0800

    update

---
+++ boruishi/MLcode/text_Con_knn_test.py
@@ -0,0 +1,68 @@
+import pandas
+import nltk
+import numpy
+import codecs
+import sys
+import pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.lancaster import LancasterStemmer
+from gensim import corpora, models, similarities
+from os import listdir
+from os.path import isfile, join
+import math
+import pickle
+
+
+class text_ope_knn_test:
+    def __init__(self):
+        self.df = pandas.read_csv('C:\\Users\\Deman\\Desktop\\FacebookDataTCSS555Project\\TCSS555\\Train\\Profile\\Profile.csv')
+
+        cf = open('C:\\Users\\Deman\\Desktop\\FacebookDataTCSS555Project\\dictionary.pickle', 'rb')
+        self.dictionary = pickle.load(cf)
+        cf.close()
+
+        cf = open('C:\\Users\\Deman\\Desktop\\FacebookDataTCSS555Project\\lsi.pickle', 'rb')
+        self.lsi = pickle.load(cf)
+        cf.close()
+
+        cf = open('C:\\Users\\Deman\\Desktop\\FacebookDataTCSS555Project\\index.pickle', 'rb')
+        self.index = pickle.load(cf)
+        cf.close()
+
+        return
+
+    def format(self, txt):
+        texts_token = nltk.word_tokenize(txt)
+        english_stopwords = stopwords.words("english")
+        texts_filtered_stopwords = [word for word in texts_token if not word in english_stopwords]
+        english_corm = ","
+        texts_filtered_corm = [word for word in texts_filtered_stopwords if not word in english_corm]
+        st = LancasterStemmer()
+        text_stemmed = [st.stem(word) for word in texts_filtered_stopwords]
+        #text = [stem for stem in txt if stem not in text_stemmed ]
+        return text_stemmed
+
+    def test(self, texts):
+        ml_course = texts
+        ml_bow = self.dictionary.doc2bow(ml_course)
+        ml_lsi = self.lsi[ml_bow]
+        sims = self.index[ml_lsi]    
+        sort_sims = sorted(enumerate(sims), key=lambda item: -item[1])
+        ope_sum = 0
+        for i in range (0,15):
+            row = sort_sims[i][0]
+            ope = self.df['con'][row]
+            ope_sum+=ope
+        ope_sum = ope_sum/15
+        return ope_sum
+        
+    def predict(self, txt): 
+        texts = self.format(txt)
+        userope = self.test(texts)
+        return userope
+
+
+
+
+
---
+++ chchao/oxford.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import sys
+import math
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import cross_val_score
+import os
+import pickle
+
+def distance(p0, p1):
+	return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+def mid(p0, p1):
+	return [(p0[0]+p1[0])/float(2), (p0[1]+p1[1])/float(2)]
+
+def oxford_feature(oxford, userid):
+	result = []
+	o = oxford.query("userId == '%s'"%userid)
+	i = 0
+	if len(o) == 0:
+		return False, []
+	elif len(o) > 1:
+		bigface = 0
+		for j in range(0, len(o)):
+			if int(o.iloc[j]['faceRectangle_width'])*int(o.iloc[j]['faceRectangle_height']) > bigface:
+				bigface = o.iloc[j]['faceRectangle_width']
+				o.iloc[j]['faceRectangle_height']
+				i = j
+
+	# face features vectorization.
+
+	# eye_len = distance([o.iloc[i]['eyeLeftOuter_x'], o.iloc[i]['eyeLeftOuter_y']], [o.iloc[i]['eyeLeftInner_x'], o.iloc[i]['eyeLeftInner_y']])
+	# eye_hight = distance([o.iloc[i]['eyeLeftTop_x'], o.iloc[i]['eyeLeftTop_y']], [o.iloc[i]['eyeLeftBottom_x'], o.iloc[i]['eyeLeftBottom_y']])/eye_len
+	# eyebrow_len = distance([o.iloc[i]['eyebrowLeftInner_x'], o.iloc[i]['eyebrowLeftInner_y']], [o.iloc[i]['eyebrowLeftOuter_y'], o.iloc[i]['eyebrowLeftOuter_y']])/eye_len
+	# outer_eye_eyebrow_len = distance([o.iloc[i]['eyebrowLeftOuter_x'], o.iloc[i]['eyebrowLeftOuter_y']], [o.iloc[i]['eyeLeftOuter_y'], o.iloc[i]['eyeLeftOuter_y']])/eye_len
+	# inner_eye_eyebrow_len = distance([o.iloc[i]['eyebrowLeftInner_x'], o.iloc[i]['eyebrowLeftInner_y']], [o.iloc[i]['eyeLeftInner_y'], o.iloc[i]['eyeLeftInner_y']])/eye_len
+	# eye_noseroot_len = distance([o.iloc[i]['eyeLeftInner_x'], o.iloc[i]['eyeLeftInner_y']], [o.iloc[i]['noseRootLeft_x'], o.iloc[i]['noseRootLeft_y']])/eye_len
+	# noseroot_len = distance([o.iloc[i]['noseRootRight_x'], o.iloc[i]['noseRootRight_y']], [o.iloc[i]['noseRootLeft_x'], o.iloc[i]['noseRootLeft_y']])/eye_len
+	# nosealartop_len = distance([o.iloc[i]['noseRightAlarTop_x'], o.iloc[i]['noseRightAlarTop_y']], [o.iloc[i]['noseLeftAlarTop_x'], o.iloc[i]['noseLeftAlarTop_y']])/eye_len
+	# nosealarout_len = distance([o.iloc[i]['noseRightAlarOutTip_x'], o.iloc[i]['noseRightAlarOutTip_y']], [o.iloc[i]['noseLeftAlarOutTip_x'], o.iloc[i]['noseLeftAlarOutTip_y']])/eye_len
+	# nose_root_alartop_len = distance(mid([o.iloc[i]['noseRootRight_x'], o.iloc[i]['noseRootRight_y']], [o.iloc[i]['noseRootLeft_x'], o.iloc[i]['noseRootLeft_y']]), mid([o.iloc[i]['noseRightAlarTop_x'], o.iloc[i]['noseRightAlarTop_y']], [o.iloc[i]['noseLeftAlarTop_x'], o.iloc[i]['noseLeftAlarTop_y']]))/eye_len
+	# nose_alartop_outtip_len = distance(mid([o.iloc[i]['noseRightAlarTop_x'], o.iloc[i]['noseRightAlarTop_y']], [o.iloc[i]['noseLeftAlarTop_x'], o.iloc[i]['noseLeftAlarTop_y']]), mid([o.iloc[i]['noseRightAlarOutTip_x'], o.iloc[i]['noseRightAlarOutTip_y']], [o.iloc[i]['noseLeftAlarOutTip_x'], o.iloc[i]['noseLeftAlarOutTip_y']]))/eye_len
+
+	# result.append(eye_hight)
+	# result.append(eyebrow_len)
+	# result.append(outer_eye_eyebrow_len)
+	# result.append(inner_eye_eyebrow_len)
+	# result.append(eye_noseroot_len)
+
+	# result.append(noseroot_len)
+	# result.append(nosealartop_len)
+	# result.append(nosealarout_len)
+	# result.append(nose_root_alartop_len	)
+	# result.append(nose_alartop_outtip_len)
+
+	result.append(o.iloc[i]['facialHair_mustache'])
+	result.append(o.iloc[i]['facialHair_beard'])
+	result.append(o.iloc[i]['facialHair_sideburns'])
+
+	return True, result
+
+
+def oxford_gender_classfier():
+	pickle_path = os.getcwd()+'/clf_gender_knn_oxford.pickle'
+
+	try:
+		cf = open(pickle_path, 'rb')
+		knn = pickle.load(cf)
+		print(knn)
+	except:
+		p = pd.read_csv('data/TCSS555/Train/Profile/Profile.csv')
+		o = pd.read_csv('data/oxford.csv')
+		features=[]
+		genders=[]
+		uid=''
+		luid=''
+		for i in range(0, len(o)):
+			uid = o.iloc[i]['userId']
+			if uid == luid:
+				continue
+			luid = uid 
+			isContainFace, feature = oxford_feature(o, uid)
+			features.append(feature)
+			genders.append(p.query("userid == '%s'"%uid).iloc[0]['gender'])
+			sys.stdout.write("preprocessing data : %4d/%04d\r"%(i,len(o)))
+			sys.stdout.flush()
+		sys.stdout.write("\n")
+
+		knn = KNeighborsClassifier(n_neighbors=10)
+		score = cross_val_score(knn, features, genders, cv=10).mean()
+		print("kNN(neighbors=10) using oxford to predict gender:")
+		print("10-fold accuracy : %f"%score)
+
+		knn.fit(features, genders)
+		print(knn)
+		
+		print('Saving classifier to '+pickle_path)
+		cf = open(pickle_path, 'wb')
+		pickle.dump(knn, cf)
+
+	return knn
+
+
